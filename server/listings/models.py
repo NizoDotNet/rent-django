6bf82ee947d9af9fd1a1b120ba38dbec2e2ad33b
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-12-15 20:14:42 +0400

    listing model

---
+++ server/listings/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from accounts.models import User 
+
+class Listing(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    city = models.CharField(max_length=15)
+    price_per_night = models.DecimalField()
+    max_guests = models.IntegerField()
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='owner'
+    )
