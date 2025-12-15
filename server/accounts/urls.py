c833560f11975177cb684f184a469a0e0c3e4900
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-12-15 17:48:00 +0400

    fixed endpoint urls

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -5,9 +5,9 @@ from .views import RefreshView
 from .views import GetRoleView
 
 urlpatterns = [
-    path('api/auth/login/', LoginView.as_view(), name='token_obtain_pair'),
-    path('api/auth/refresh/', RefreshView.as_view(), name='token_refresh'),
-    path('api/auth/register', RegisterView.as_view(), name='register_user'),
-    path('api/auth/role', GetRoleView.as_view(), name='get_role')
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
+    path('refresh/', RefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register_user'),
+    path('role/', GetRoleView.as_view(), name='get_role')
 
 ]
--- server/rent/urls.py
+++ server/rent/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('auth/', include('accounts.urls'))
+    path('api/auth/', include('accounts.urls'))
 ]
