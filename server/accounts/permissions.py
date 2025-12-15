46f6bf7e5fe066715765ced311cf0b9893ecdd66
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-12-15 17:43:30 +0400

    Customer perssion and check if user is authenticated

--- server/accounts/permissions.py
+++ server/accounts/permissions.py
@@ -2,4 +2,9 @@ from rest_framework.permissions import BasePermission
 
 class LandlordPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'landlord'
+        return request.user.is_authenticated and request.user.role == 'landlord'
+    
+
+class CustomerPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'customer'
