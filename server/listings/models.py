9d96af094e3f1f7616f2af831ce9f5f0dc6951a1
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-12-16 00:34:51 +0400

    added some validation. need add permision

--- server/listings/models.py
+++ server/listings/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from accounts.models import User 
-
+from django.core.validators import MinValueValidator, MinLengthValidator
 class Listing(models.Model):
     id = models.BigAutoField(primary_key=True)
-    title = models.CharField(max_length=20)
-    description = models.CharField(max_length=200)
-    city = models.CharField(max_length=15)
-    price_per_night = models.DecimalField(max_digits=5, decimal_places=2)
+    title = models.CharField(max_length=20, validators=[MinLengthValidator(2)])
+    description = models.CharField(max_length=200,  validators=[MinLengthValidator(2)])
+    city = models.CharField(max_length=15, validators=[MinLengthValidator(2)])
+    price_per_night = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
     max_guests = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(
