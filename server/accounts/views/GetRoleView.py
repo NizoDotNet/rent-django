ebd41b51f7c90b8cfb135b8275ad42a54ee8bb9e
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-12-15 17:41:10 +0400

    landlord permission

--- server/accounts/views/GetRoleView.py
+++ server/accounts/views/GetRoleView.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import rest_framework.status as status
+from ..permissions import LandlordPermission
 
 class GetRoleView(APIView):
+    permission_classes = [LandlordPermission]
     def get(self, request):
         return Response({ 'role': request.user.role }, status=status.HTTP_200_OK)
