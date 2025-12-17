a8235a758ccc1c9460943a8a76b4a44f00527524
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-12-17 18:44:37 +0400

    docker-compose with postgre

--- server/rent/settings.py
+++ server/rent/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'rent.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('HOST'),  
+        'PORT': '5432',       
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
